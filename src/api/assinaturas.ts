b05c12e5ec56dcc1cdd7a90ade1bb158560246f1
Erik.silva <erikkaue@gmail.com>
2025-04-25 21:42:53 -0300

    refactoring das apis

--- src/api/assinaturas.ts
+++ src/api/assinaturas.ts
@@ -1,11 +1,4 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    headers: {
-        Authorization: localStorage.getItem("token") || "",
-    },
-});
+import { api } from "./api";
 
 export interface TotaisResponse {
     inicio: string;

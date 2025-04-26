4e6d842a6ca86f7cc8208faa6bc33724a61e1eac
Erik.silva <erikkaue@gmail.com>
2025-04-25 21:55:39 -0300

    Updates

--- src/utils/formatDate.ts
+++
@@ -1,4 +0,0 @@
-export const formatarDataBR = (dataIso: string): string => {
-    const [ano, mes, dia] = dataIso.split("-");
-    return `${dia}/${mes}/${ano}`;
-};
--- src/utils/formatMoney.ts
+++
@@ -1,3 +0,0 @@
-export const formatarDinheiroBR = (valor: number): string => {
-    return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-};
